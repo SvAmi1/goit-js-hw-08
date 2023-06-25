617a7c2344614b689a6bc099746348c527596f4a
SvAmi1 <svmoshko877@gmail.com>
2023-06-25 11:49:38 +0300

    update video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,7 +2,7 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 const STORAGE_KEY = "videoplayer-current-time";
 
@@ -10,9 +10,8 @@ player.on('timeupdate', throttle(getCurrentTime, 1000));
 
 setStorage();
 
-function getCurrentTime(ev) {
-    const currentTime = ev.seconds;
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(currentTime));
+function getCurrentTime(currentTime) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(currentTime.seconds));
 }
 
 function setStorage() {
